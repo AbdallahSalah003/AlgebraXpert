00126e234b5a0b9a39a8eac8856d9e713fcd6814
AbdallahSalah003 <abdallahsalahsalem9@gmail.com>
2024-09-16 01:27:32 +0300

    Refactor run tests script

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,20 +1,20 @@
-cd test_linear_equation_solver/
-qmake > /dev/null 2>&1
-make > /dev/null 2>&1
-./test_linear_equation_solver
-make clean > /dev/null
-rm Makefile test_linear_equation_solver > /dev/null
+#!/bin/bash
 
-cd ../test_matrix/
-qmake > /dev/null 2>&1
-make > /dev/null 2>&1
-./test_matrix
-make clean > /dev/null
-rm Makefile test_matrix > /dev/null
+run_tests() {
+    local test_dir=$1
+    local executable=$2
 
-cd ../test_quadratic_equation_solver/
-qmake > /dev/null 2>&1
-make > /dev/null 2>&1
-./test_quadratic_equation_solver
-make clean > /dev/null
-rm Makefile test_quadratic_equation_solver > /dev/null
+    cd $test_dir || exit
+    qmake > /dev/null 2>&1
+    make > /dev/null 2>&1
+    ./$executable
+    make clean > /dev/null
+    rm Makefile $executable > /dev/null
+    cd - > /dev/null || exit
+}
+
+run_tests "test_linear_equation_solver" "test_linear_equation_solver"
+run_tests "test_matrix" "test_matrix"
+run_tests "test_quadratic_equation_solver" "test_quadratic_equation_solver"
+run_tests "test_system_solver" "test_system_solver"
+
